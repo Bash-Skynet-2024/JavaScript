f6812c7a6e3a1dc948039788204dab66c3d5c2a5
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 13:26:49 +0530

    Create JS9.js

---
+++ JS9.js
@@ -0,0 +1,10 @@
+// object printing ibdividuallu
+let data ={
+    " aa" : 34,
+    "bb": 45,
+    "cc": 56,
+    "dd": 67
+};
+for (let i in data){
+    console.log("marks of "+i+" are "+ data[i]);
+}
