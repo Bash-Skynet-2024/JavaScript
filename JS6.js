575d23ab9fe4c4493308e9a8b34d98a5ed9d1169
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 11:38:40 +0530

    Create JS6.js

---
+++ JS6.js
@@ -0,0 +1,9 @@
+// whether age is between 10 and 20
+let age = prompt("enter ur age : ");
+if (age < 10){
+    alert("ur a kiddo");
+}
+else if ( age > 10 && age < 20){
+    alert("ur adult");
+}
+else { alert("ur old"); }
