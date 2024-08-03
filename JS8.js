fc807af34b14a22937817da15934f6981aef7433
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 11:56:29 +0530

    Update JS8.js

--- JS8.js
+++ JS8.js
@@ -1,7 +1,19 @@
-// num divisibility by 2 and 3
+// num divisibility by 2-9 / print seperate for all div
 let num = prompt("enter number : ");
 for(let i = 2 ; i < 10; ++i){
     if ( num % i == 0){
         alert("\n"+num+" is divisible by "+i);
     }
 }
+// modifications : print all div at same time
+// num divisibility by 2 and 3
+let num = prompt("enter number : ");
+let arr = [];
+let b = 0;
+for(let i = 2 ; i < 10; ++i){
+    if ( num % i == 0){
+        arr[b]= i;
+        ++b;
+    }
+}
+alert("\n"+num+" is divisible by : "+ arr);
