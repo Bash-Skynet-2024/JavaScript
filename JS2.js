3bac5fa83e59c0105c8088e73068a1e8018e9e04
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 09:43:20 +0530

    Create JS2.js

---
+++ JS2.js
@@ -0,0 +1,16 @@
+// getting data type of variables
+let a = "hello";
+let b = 45;
+let c = true;
+let d = BigInt("567");
+let e = [ 1,2,3];
+let f = {
+    num : 45,
+    name : "js"
+};
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
