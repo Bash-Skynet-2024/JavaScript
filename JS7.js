9774b423d8a7965266830521735933ed252e43f8
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 11:42:49 +0530

    Create JS7.js

---
+++ JS7.js
@@ -0,0 +1,10 @@
+//  using switch
+let a = prompt(" enter colour : ");
+switch (a){
+    case "red" : alert(" stop");
+    break;
+    case "yellow" : alert(" go slow");
+    break;
+    case "green" : alert("go");
+    break;
+}
