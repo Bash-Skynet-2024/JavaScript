0e03762171d9ec66afc906717d5e1d10aea80293
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 11:18:04 +0530

    Create JS5.js

---
+++ JS5.js
@@ -0,0 +1,8 @@
+// input output - browser
+// input num from user using dialogue box of browser and return it's square
+alert(" enter num u want to square in next dialogue box");
+// alert used to print only
+let a = prompt("enter num "); // prompt used to display txt and input
+alert("square of "+a+" is "+a*a);// dont use ',' use '+'
+// fun prog.
+prompt("Are you 18+ ? \n y/n ");
