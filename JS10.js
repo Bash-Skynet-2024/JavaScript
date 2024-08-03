1591612445141eda22f2cd14afbf043369961153
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 13:37:25 +0530

    Create JS10.js

---
+++ JS10.js
@@ -0,0 +1,8 @@
+// sum of n natural nums
+console.log("program to calc sum of n natural numbers ");
+let n = prompt("enter n : ");
+let sum = 0;
+for (let i = 1; i<= n ; i++){
+    sum += i ;
+}
+console.log("sum of "+n+" natural nums is "+sum);
