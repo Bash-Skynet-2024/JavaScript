56a7bdd065415b64aa3b6792f4387e3217c970a8
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 09:48:47 +0530

    Create JS3.js

---
+++ JS3.js
@@ -0,0 +1,17 @@
+// create an object and make changes later
+let a = [ 1,2,3,4,5,6];
+console.log(a);
+a[2]= 2;
+console.log(a);
+let b = {
+    name : "js",
+    num : 1,
+    "Name" : "coder",
+    "roll" : 4
+};
+console.log(b);
+b.name = "coder";
+b.num = 4;
+b["Name"] = "js";
+b["roll"]= 1;
+console.log(b);
