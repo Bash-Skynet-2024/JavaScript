f4e9163708d89a11f733bbbfd77c1da5ac2f424f
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2024-08-03 10:26:54 +0530

    Create JS4.js

---
+++ JS4.js
@@ -0,0 +1,14 @@
+// arithmstic opeartors
+let a = 2;
+let b = 3
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+console.log(b%a);
+console.log(++a); // first inc. then print : a= 2+1 = 3 : output 3
+console.log(a++); // first print then inc : output 3 : a = 3+1=4
+console.log(--a); // first dec, then print : a= 4-1=3 : output 3
+console.log(a--); // first print then dec. : output 3 : a=3-1=2
+console.log(a);
